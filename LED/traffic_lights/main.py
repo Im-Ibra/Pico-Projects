f24f476f1a44ca1789b7457b47b0ab544d006f7d
Ibra <ibra.garabajiu@gmail.com>
2025-10-17 14:11:35 +0200

    Amended the last push

--- LED/traffic_lights/main.py
+++ LED/traffic_lights/main.py
@@ -1,12 +1,21 @@
 from machine import Pin
 import time
 
-led = Pin(0, Pin.OUT)
-sensor = Pin(28, Pin.IN)
- 
+#Red led connected to GPIO 5
+r_led = Pin(5, Pin.OUT)
+
+#Yellow led connected to GPIO 9
+y_led = Pin(9, Pin.OUT)
+
+#Green led connected to GPIO 13
+g_led = Pin(13, Pin.OUT)
+
+#List with all leds
+traffic_light =  [g_led, y_led, r_led]
+
 while True:
-    if sensor.value() == 1:
-        led.value(1)
-    else:
-        led.value(0)
-    time.sleep(.1)
+    #Loops through all the leds inside the traffic_light list
+    for leds in traffic_light:
+        leds.value(1)
+        time.sleep(1)
+        leds.value(0)
